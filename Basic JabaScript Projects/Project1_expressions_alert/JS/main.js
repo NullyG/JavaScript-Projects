b4a6120ffb9c576a6472192e9d262970c57430fe
NullyG <86624542+NullyG@users.noreply.github.com>
2021-08-16 15:49:57 -0400

    created document and added JS

---
+++ Basic JabaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,18 @@
+window.alert("Hello, World!")
+document.write("Hello, World!")
+var A = "This is a string";
+document.write(A);
+var B = "this is my variable"
+window.alert(B)
+document.write(" nobody ever said \"escaping in code was useless\"") //prints the string after document.write
+document.write ("\"Be who you are and say what you feel,"
++" because those who mind don\'t matter and those who matter don\'t mind.\""
++"-Dr. Seuss"); //prints the concatenated strings found after document.write
+var D = "Concatenated" + " String"
+document.write(D) //concatenates the strings in var D
+var Family = "The Arezzinis", Dad = "Jeremiah", Mom = "hermoine", Daughter = "Penny", Son = "Zorro";
+document.write(Dad); //created variables for the Arezzinis family and printed one of them in the document
+var blues = "I have the blues.";
+var blues = blues.fontcolor("blue"); //this prints blue font contained in the string "blues"
+document.write(blues);
+document.write(3+3); //this prints the result of 3+3
