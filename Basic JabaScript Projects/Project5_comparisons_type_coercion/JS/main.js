0a62a5870d657044d7709b9236312d436452530f
NullyG <86624542+NullyG@users.noreply.github.com>
2021-08-23 14:34:58 -0400

    added coments

--- Basic JabaScript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JabaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -7,28 +7,35 @@ document.write("10" + 5);
 function big_Number(){
 document.getElementById("Number").innerHTML(3E310);
 }
-document.write(10 < 2);
-document.write(10 > 2);
-console.log(2 + 2 == 5);
-document.write(10 == 10);
+document.write(10 < 2); //creates a less than statement that returns false
+
+document.write(10 > 2); //creates a greater than statement that returns true
+
+console.log(2 + 2 == 5); //creates an equal statement that returns false
+
+document.write(10 == 10); //creates an equal statement that returns true
 X = 82;
+
 Y = 82;
 document.write(X === Y);
 Y = "82";
+
 document.write(X === Y);
 Y = "83";
-document.write(X === Y);
-Y = 83;
-document.write(X === Y);
 
-document.write(5>10 || 4 > 10);
+document.write(X === Y);
+Y = 83; 
 
-document.write(5>10 || 10 > 4);
+document.write(X === Y); 
+
+document.write(5>10 || 4 > 10); //creates an or statement that returns false
+
+document.write(5>10 || 10 > 4); //creates an or statement that returns true
 
 function not_Function() {
-    document.getElementById("Not").innerHTML = ! (20 > 10);
+    document.getElementById("Not").innerHTML = ! (20 > 10); //creates a not function that returns false
 }
 
 function not_Function_2() {
-    document.getElementById("Not_2").innerHTML = ! (5 > 10);
+    document.getElementById("Not_2").innerHTML = ! (5 > 10); //creates a not function that returns true
 }
